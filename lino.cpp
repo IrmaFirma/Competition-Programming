1f95930c33f38ad668ef5b428bf0e975117e52bb
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2021-04-29 13:30:57 +0200

    Create lino.cpp

---
+++ lino.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int
+main ()
+{
+  int n;
+  cin >> n;
+  string gol[n];
+  for (int i = 0; i < n; i++)
+    {
+      cin >> gol[i];
+    }
+
+  int br_hr = 0;
+  int br_s = 0;
+  int max = 0;
+  int razlika = br_hr - br_s;
+  for (int i = 0; i < n; i++){
+      if (gol[i] == "H"){
+	  br_hr++;
+	  razlika = br_hr-br_s;
+	  if(razlika > max){
+	      max=max+(razlika)+1;
+	  }
+	}else{
+	  br_s++;
+	  razlika = br_hr-br_s;
+	   if(razlika > max){
+	      max=max+(razlika)+1;
+	  }
+	}
+}
+
+  cout << br_hr << endl;
+  cout << max;
+
+  return 0;
+}
