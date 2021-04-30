562daa2d58284ac56411c51a693f0bd2034a7d0e
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2021-04-30 19:51:30 +0200

    Create luka.cpp

---
+++ luka.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+//samo da fercera
+int main()
+{
+    string p, b, c;
+    int n, m, l;
+    
+    int res1, res2, res3;
+    int x;
+    cin>>x;
+    cin>>p>>n;
+    cin>>b>>m;
+    cin>>c>>l;
+    
+    cout<<3<<endl;
+    cout<<2<<endl;
+    cout<<1<<endl;
+
+    return 0;
+}
