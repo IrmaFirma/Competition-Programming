11981cb6507eba096c605e46e5ed57940775f97b
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2021-08-02 17:05:53 +0200

    Create 99bottlespop.cpp

---
+++ 99bottlespop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+  // Write a for loop here:
+  for(int i = 100; i > 0; i--)
+  {
+    std::cout<<i<<" bottles of pop on the wall.\n";
+    std::cout<<"Take one down and pass it around.\n";
+  }
+  std::cout << "No more bottles of pop on the wall.\n";
+  std::cout << "No more bottles of pop.\n";
+  std::cout << "Go to the store and buy some more,\n";
+  std::cout << "99 bottles of pop on the wall.\n";
+}
