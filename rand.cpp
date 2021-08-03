bf938c0dfe67c091fa583a456f217b6e3378d1a3
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2021-08-03 20:20:51 +0200

    Create rand.cpp

---
+++ rand.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  
+  // This seeds the random number generator:
+  srand (time(NULL));
+  int the_amazing_random_number = rand() % 8;
+  std::cout<<the_amazing_random_number<<"\n";
+  
+  // Use rand() below to initialize the_amazing_random_number
+  
+  
+}
