472abc38686dd25bafd4d63e4ec1af7f418a4bcf
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2021-08-03 15:41:02 +0200

    Update nyc.js

--- nyc.js
+++ nyc.js
@@ -1,3 +1,8 @@
 var myName = 'Irma';
 var myCity = 'New York City';
 console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
