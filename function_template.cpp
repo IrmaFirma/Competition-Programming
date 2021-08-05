a42e8857aaee9381d1adb9512eb41cc3206a8a03
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2021-08-05 11:00:46 +0200

    Create function_template.cpp

---
+++ function_template.cpp
@@ -0,0 +1,7 @@
+template <typename T>
+void print_cat_ears(T item) {
+ 
+  std::cout << " " << item << "   " << item << " " << "\n";
+  std::cout << item << item << item << " " << item << item << item << "\n";
+ 
+}
