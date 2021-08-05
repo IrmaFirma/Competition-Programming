1b281b3b335bf05377a6a9d051e078172d332c7c
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2021-08-05 11:53:38 +0200

    Update conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -1,9 +1,14 @@
-let sale = true;
+let season = 'summer';
 
-sale = false;
-
-if(sale) {
-  console.log('Time to buy!');
-}else{
-  console.log("Time to wait for a sale.");
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} else if(season === 'winter'){
+  console.log('It\'s winter! Everything is covered in snow.');
+}else if(season === 'fall'){
+  console.log('It\'s fall! Leaves are falling!');
+}else if(season === 'summer'){
+  console.log('It\'s sunny and warm because it\'s summer!');
+}else {
+  console.log('Invalid season.');
 }
+
