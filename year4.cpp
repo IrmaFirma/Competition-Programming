34004b69670d053297e150c81d91d0e86d0ec973
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2022-04-16 17:50:00 +0200

    Create year4.cpp

---
+++ year4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    cin>>x;
+    
+    if(x%4==0) cout<<"Prestupna je godina.";
+    else cout<<"Nije prestupna godina.";
+    
+    return 0;
+}
