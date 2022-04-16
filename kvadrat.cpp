6e948342e424b13703873bbf3e04fb7a1bfaff3f
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2022-04-16 12:55:02 +0200

    Create kvadrat.cpp

---
+++ kvadrat.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin>>a;
+    int obim = 4*a;
+    int povrsina = a*a;
+    cout<<obim<<" "<<povrsina;
+
+	return 0;
+}
