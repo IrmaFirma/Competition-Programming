c36ac73be0230d548252872609f3f00ed42c8e91
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2022-04-16 12:51:53 +0200

    Create brojevi.cpp

---
+++ brojevi.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin>>a>>b;
+    if(a>b) cout<<a;
+    else if(a<b) cout<<b;
+    else cout<<"JEDNAKI";
+
+	return 0;
+}
