cab82f80eb9427169d96f1eda16baeec62f930de
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2022-04-18 19:31:52 +0200

    Create printprimes.cpp

---
+++ printprimes.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+ Bilten 2006 II zadatak jedan
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main()
+{
+    int n, m, jeProst;
+    cin>>n>>m;
+    
+    for(int i = n; i<=m; i++){
+        jeProst=1;
+        for(int j = 2; j<=i/2; j++){
+            if(i%j==0){
+                jeProst=0;
+                break;
+            }
+        }
+        if(jeProst==1) cout<<i<<" ";
+    }
+    
+    return 0;
+}
