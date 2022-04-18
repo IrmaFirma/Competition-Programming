553d70c4b342edb5a7bdd6767c2eb6acd01e3fab
Irma Preldzic <66248314+IrmaFirma@users.noreply.github.com>
2022-04-18 17:54:03 +0200

    Create num_digits.cpp

---
+++ num_digits.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+ Bilten 2018 II zadatak jedan
+
+*******************************************************************************/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    string s = to_string(n);
+    int res = s.length();
+    
+    cout<<res;
+
+    return 0;
+}
